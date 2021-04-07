c956cdb5822fc5fdc851dd17aa3d2cdd6174d419
jmuhic <jmuhic@umich.edu>
2021-04-07 12:26:13 -0400

    sample fda search

--- drugs.py
+++ drugs.py
@@ -1,7 +1,20 @@
 import requests
 import json
 import webbrowser
+import secret_drugs
 
+def find_adverse_reactions(drug_name):
+    fda_url_base = "https://api.fda.gov/drug/event.json?"
+    api_key = FDA_API_KEY
+    
+    '''
+    search by either brand name first, if not found, then search generic.
+    if nothing found, then print apologies and ask to try another drug search
+    #fda_url_search = 
+    #https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:%22BONIVA%22
+    #search=patient.drug.openfda.generic_name:"FOO+BAR"
+    '''
+    pass
 
 
 
