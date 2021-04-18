c5ace1be03a80414a5ec9b8a21f480369de80a6b
jmuhic <jmuhic@umich.edu>
2021-04-17 20:47:49 -0400

    added summary functions and tables

--- drugs.py
+++ drugs.py
@@ -190,16 +190,141 @@ def results_loop_reactions(raw_data, user_reaction):
     return results_list
 
 
+def total_reaction_by_drug(drug_name):
+    '''
+    Returns a summarized list of rections reported to the FDA
+    for the drug entered by the user.
+    Will return data from cache, if found.  Otherwise,
+    will use FDA API to retrieve the information.
+
+    Parameters:
+    -----------
+    drug_name: string
+        name of drug entered by user
+
+    Returns: 
+    --------
+    None (writes results to table in DB)
+    '''
+    drug_name = drug_name.upper()
+    json_dict = {}
+    json_dict = check_summary_cache(drug_name)
+
+    if json_dict:
+        tot_reactions = json_dict['results']
+        summary_list = []
+        for i in range(len(tot_reactions)):
+            reaction = tot_reactions[i]['term']
+            count = tot_reactions[i]['count']
+            summary_list.append((drug_name, reaction, count))
+
+    elif json_dict is None:
+        summary_url_base = "https://api.fda.gov/drug/event.json?api_key="
+        api_key = secret_drugs.FDA_API_KEY
+        descrip = '&count=patient.reaction.reactionmeddrapt.exact&search=patient.drug.medicinalproduct.exact:'
+
+        # Getting data from FDA API Call
+        # Call returns the top 100 reactions reported to the FDA
+        output = requests.get(summary_url_base + api_key + descrip + drug_name)
+        json_dict = json.loads(output.text)
+
+        try:
+            tot_reactions = json_dict['results']
+            add_to_summary_cache(drug_name, json_dict)
+
+        # Building a dictionary to list reporting reactions and number of occurrences
+            summary_list = []
+            for i in range(len(tot_reactions)):
+                reaction = tot_reactions[i]['term']
+                count = tot_reactions[i]['count']
+                summary_list.append((drug_name, reaction, count))
+            # results_list = []
+            # for i in range(len(reactions)):
+            #     for x in range(len(reactions[i]['patient']['reaction'])):
+            #         drug_reaction = reactions[i]['patient']['reaction'][x]['reactionmeddrapt']
+            #         report_id = reactions[i]['safetyreportid']
+            #         results_list.append((report_id, drug_name, drug_reaction))
+        except:
+            print('Drug not found in FDA database. Please try another search.')
+            return None
+
+    write_Reaction_DB(summary_list)
+
+
+def total_drugs_by_reaction(reaction):
+    '''
+    Returns a summarized list of drugs by count for the
+    reaction entered by the user.
+    Will return data from cache, if found.  Otherwise,
+    will use FDA API to retrieve the information.
+
+    Parameters:
+    -----------
+    reaction_name: string
+        name of reaction entered by user
+
+    Returns:
+    --------
+    None (writes results to table in DB)
+    '''
+    reaction = reaction.upper()
+    json_dict = {}
+    json_dict = check_summary_cache(reaction)
+
+    if json_dict:
+        tot_drugs = json_dict['results']
+        summary_list = []
+        for i in range(len(tot_drugs)):
+            drug_name = tot_drugs[i]['term']
+            count = tot_drugs[i]['count']
+            summary_list.append((drug_name, reaction, count))
+
+    elif json_dict is None:
+        summary_url_base = "https://api.fda.gov/drug/event.json?api_key="
+        api_key = secret_drugs.FDA_API_KEY
+        descrip = '&count=patient.drug.medicinalproduct.exact&search=patient.reaction.reactionmeddrapt.exact:'
+
+        # Getting data from FDA API Call
+        # Call returns the top 100 instances reported to the FDA
+        output = requests.get(summary_url_base + api_key + descrip + reaction)
+        json_dict = json.loads(output.text)
+
+        try:
+            tot_drugs = json_dict['results']
+            add_to_summary_cache(reaction, json_dict)
+
+        # Building a dictionary to list reporting reactions and number of occurrences
+            summary_list = []
+            for i in range(len(tot_drugs)):
+                drug_name = tot_drugs[i]['term']
+                count = tot_drugs[i]['count']
+                summary_list.append((drug_name, reaction, count))
+            # results_list = []
+            # for i in range(len(reactions)):
+            #     for x in range(len(reactions[i]['patient']['reaction'])):
+            #         drug_reaction = reactions[i]['patient']['reaction'][x]['reactionmeddrapt']
+            #         report_id = reactions[i]['safetyreportid']
+            #         results_list.append((report_id, drug_name, drug_reaction))
+        except:
+            print('Drug not found in FDA database. Please try another search.')
+            return None
+
+    write_Drug_DB(summary_list)
+
 def create_database():
     '''
     Creates the database and tables that will be used to store
     the results from the user's search(es).  If the tables/database
     already exists, there will be no update.
-    There are three tables:
+    There are five tables:
     (1) Drugs - table of all drugs searched by user
     (2) Reactions - table of all reactions searched by user
     (3) Drug_Reactions - table of drugs and associated reactions
         in addition to the FDA's report id for the drug/reaction combo
+    (4) Reactions_per_Drug - table of drugs that list the number of times
+        a reaction has been reported for a specified drug
+    (5) Drug_per_Reaction - table that lists the number of times a Drug
+        has been reported for a specified Reaction
 
     Parameters:
     -----------
@@ -246,6 +371,91 @@ def create_database():
     '''
     )
 
+    # Create the Reaction Summary table if does not already exist
+    # List count of reactions reported for a specified drug
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS "Reactions_per_Drug" (
+	"Drug"	TEXT NOT NULL,
+	"Reaction"	TEXT NOT NULL,
+	"Reaction_Count"	INTEGER NOT NULL
+    )
+    '''
+    )
+
+    # Create the Drug Summary table if does not already exist
+    # List count of Drugs reported for a specified Reaction
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS "Drug_per_Reaction" (
+	"Reaction"	TEXT NOT NULL,
+	"Drug"	TEXT NOT NULL,
+	"Drug_Count"	INTEGER NOT NULL
+    )
+    '''
+    )
+
+def write_Reaction_DB(summary_list):
+    '''
+    Writes to the database the results returned for the
+    number of Reactions reported for a particular Drug
+
+    Parameters:
+    -----------
+    summary_list: list
+        The list of results returned with the count of reactions
+        for a specified drug.
+
+    Returns:
+    --------
+    None
+    '''
+    #create_database()
+
+    # Re-connect to database
+    conn = sqlite3.connect('FDA_DRUGS.db')
+
+    # Re-create DB cursor
+    cur = conn.cursor()
+
+    for i in range(len(summary_list)):
+        cur.execute(f"INSERT OR IGNORE INTO Reactions_per_Drug VALUES\
+            ('{summary_list[i][0]}', '{summary_list[i][1]}',\
+                '{summary_list[i][2]}')")
+    conn.commit()
+
+    # Close the connection to the database
+    conn.close()
+
+def write_Drug_DB(summary_list):
+    '''
+    Writes to the database the results returned for the
+    number of times a Drug was reported for a particular Reaction
+
+    Parameters:
+    -----------
+    summary_list: list
+        The list of results returned with the report count of a Drug
+        reported for a specified Reaction.
+
+    Returns:
+    --------
+    None
+    '''
+    #create_database()
+
+    # Re-connect to database
+    conn = sqlite3.connect('FDA_DRUGS.db')
+
+    # Re-create DB cursor
+    cur = conn.cursor()
+
+    for i in range(len(summary_list)):
+        cur.execute(f"INSERT OR IGNORE INTO Drug_per_Reaction VALUES\
+            ('{summary_list[i][1]}', '{summary_list[i][0]}',\
+                '{summary_list[i][2]}')")
+    conn.commit()
+
+    # Close the connection to the database
+    conn.close()
 
 def write_to_DB(user_search, search_results, search_type):
     '''
@@ -272,7 +482,7 @@ def write_to_DB(user_search, search_results, search_type):
     --------
     None
     '''
-    create_database()
+    #create_database()
 
     # Re-connect to database
     conn = sqlite3.connect('FDA_DRUGS.db')
@@ -328,10 +538,8 @@ def check_cache(key):
         content of key in dict, if found
     '''
     if key in json_cache:
-        #print('Using cache')
         return json_cache[key]
     else:
-        #print('Fetching')
         return None
 
 
@@ -355,8 +563,52 @@ def add_to_cache(key, value):
     json_cache[key] = value
 
     # writing new key,value pair to cache file
-    with open("drugs_cache_redo.json", "w") as cache:
+    with open("drugs_cache.json", "w") as cache:
         json.dump(json_cache, cache, indent=2)
+
+
+def check_summary_cache(key):
+    '''
+    Checks the cache to see if the data has already been run
+    and stored in cache.  Returns value if found.
+
+    Parameters:
+    -----------
+    key: string
+        key from key,value pair in cache
+
+    Returns:
+    --------
+    value: string
+        content of key in dict, if found
+    '''
+    if key in json_summary_cache:
+        return json_summary_cache[key]
+    else:
+        return None
+
+def add_to_summary_cache(key, value):
+    '''
+    Adds key, value pair to json_cache file
+
+    Parameters:
+    -----------
+    key: string
+        key from key,value pair in cache
+
+    value: string
+        contents of key
+
+    Returns:
+    --------
+    None
+    '''
+    # creating key,value pair for cache file
+    json_summary_cache[key] = value
+
+    # writing new key,value pair to cache file
+    with open("summary_cache.json", "w") as summary_cache:
+        json.dump(json_summary_cache, summary_cache, indent=2)
 
 
 def create_table(fda_results, search_type, user_search):
@@ -403,21 +655,28 @@ def create_table(fda_results, search_type, user_search):
 
 # Initializing setup of cache
 json_cache = {}
-path = 'drugs_cache_redo.json'
+path = 'drugs_cache.json'
+
+json_summary_cache = {}
+summary_path = 'summary_cache.json'
 
 # if the cache file exist, read from that file
 if os.path.isfile(path):
     with open('drugs_cache_redo.json') as f:
         json_cache = json.load(f)
 
+if os.path.isfile(summary_path):
+    with open('summary_cache.json') as sum_f:
+        json_summary_cache = json.load(sum_f)
+
 
 if __name__ == "__main__":
     # # interactive search should go here
-    drug_test = find_by_reaction('headache')
+    #drug_test = find_by_reaction('headache')
     #drug_test = drug_test.capitalize()
-    print(drug_test)
+    #print(drug_test)
 
-    write_to_DB('headache', drug_test, 'reaction')
+    #write_to_DB('headache', drug_test, 'reaction')
 
     # while True:
     #     drug_search = input("Please enter the name of a drug to search: ")
@@ -436,4 +695,7 @@ if __name__ == "__main__":
 
         # # For Reddit section
         # more_info = input("Would you like to find out more info about this drug (y/n)? ")
+    create_database()
+    #total_reaction_by_drug("Tylenol")
+    total_drugs_by_reaction("cough")
     exit()
